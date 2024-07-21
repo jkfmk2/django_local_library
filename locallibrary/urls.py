f4ac68fcc17d8e824b0b52d2563adab2eee69551
jkfmk2 <jkfmk2@hotmail.com>
2024-07-21 20:21:58 +0800

    add registration templates

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -26,6 +26,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Add Django site authentication urls (for login, logout, password management)
+    path('accounts/', include('django.contrib.auth.urls')),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True),)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
